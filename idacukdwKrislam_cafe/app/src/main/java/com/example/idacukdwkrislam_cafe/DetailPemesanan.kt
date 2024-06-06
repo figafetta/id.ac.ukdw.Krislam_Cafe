e772dd2956408c5d51d56a58236da6afae4d63a4
GustyFachtaPratama <112554244+GustyFachtaPratama@users.noreply.github.com>
2024-06-07 03:00:12 +0700

    konek pemesanan to detailPemesanan

---
+++ idacukdwKrislam_cafe/app/src/main/java/com/example/idacukdwkrislam_cafe/DetailPemesanan.kt
@@ -0,0 +1,20 @@
+package com.example.idacukdwkrislam_cafe
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+class DetailPemesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail_pemesanan)
+
+        // Ambil referensi ke ImageButton back
+        val buttonBack = findViewById<ImageButton>(R.id.buttonback)
+
+        // Tetapkan listener untuk ImageButton back
+        buttonBack.setOnClickListener {
+            // Tutup aktivitas ini dan kembali ke aktivitas sebelumnya
+            finish()
+        }
+    }
+}
