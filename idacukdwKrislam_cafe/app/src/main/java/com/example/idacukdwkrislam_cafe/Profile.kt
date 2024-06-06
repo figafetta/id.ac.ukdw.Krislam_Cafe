76f2235f0de49f3946f7043ff2f0ed4f5aeeba71
Figafetta Temminckii <figa.fetta@ti.ukdw.ac.id>
2024-06-07 02:49:48 +0700

    add fitur
    menambahkan email pada profile sesuai dengan email yang dimasukkan

--- idacukdwKrislam_cafe/app/src/main/java/com/example/idacukdwkrislam_cafe/Profile.kt
+++ idacukdwKrislam_cafe/app/src/main/java/com/example/idacukdwkrislam_cafe/Profile.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import android.content.Intent
 
@@ -24,6 +25,7 @@ class Profile : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private lateinit var auth: FirebaseAuth
+    private lateinit var textViewEmail: TextView
     private lateinit var buttonLogout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +43,16 @@ class Profile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        textViewEmail = view.findViewById(R.id.textView_email_profile)
         buttonLogout = view.findViewById(R.id.button_keluar)
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val email = currentUser.email
+            textViewEmail.text = email
+        } else {
+            textViewEmail.text = "User not logged in"
+        }
 
         buttonLogout.setOnClickListener {
             auth.signOut()
@@ -52,7 +63,4 @@ class Profile : Fragment() {
 
         return view
     }
-
-
-
 }
